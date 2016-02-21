3f3a1f318c63fa6b7655fee8e6e19c13eba1ed0c
tizphani <iamphani@gmail.com>
2016-02-20 21:23:29 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+	## makeCacheMatrx and CacheSolve functions are used to calculate and
+	## cache the inverse of a matrix. Once the inverse is cached subsequent
+	## calls on the same matrix will not result in calculation of the
+	## inverse. Instead, the cached value is returned
+	makeCacheMatrix <-function(matr = matrix()){
+		rev <- NULL
+		get <- function() matr
+		set <- function(m){
+		## override the values of matrix and its inverse to NULL
+			rev <<- NULL
+			matr <<- m
+		}
+		getrev <- function() rev
+		setrev <- function(m) rev <<-m
+		## creating the list to subset the functions
+		list(set=set,get=get,getrev=getrev,setrev=setrev)
+	}
+	## function to calculate inverse of a matrix stored in x
+	## if the inverse is already available it retrieved from
+	## the cache
+	cacheSolve <- function(x,...){
+		rev <- x$getrev()
+		## if the inverse is available retrieve the cached value
+		if(!is.null(rev)){
+			message("getting cached data...")
+			return(rev)
+		}
+		##calculate the inverse and return it
+		matr <- x$get()
+		rev <- solve(matr)
+		x$setrev(rev)
+		rev
+	}
